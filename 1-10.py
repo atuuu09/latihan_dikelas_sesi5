5951499882bc35d00f00825a1710c0d4547a7382
atuuu09 <160223872+atuuu09@users.noreply.github.com>
2024-03-15 15:41:47 +0700

    Add files via upload

---
+++ 1-10.py
@@ -0,0 +1,63 @@
+#soal latihan
+#1. 10 9 8 7 6 5 4 3 2 1
+#for i in range(10,0,-1):
+#    print(i,end=" ")
+    
+# 2. 1 3 5 7 9 11 13 15 17 19
+#for i in range(1,20,2):
+#    print(i,end=" ")
+ 
+#3. 25 20 15 10 5 0
+#for i in range(25,-1,-5):
+#    print(i,end=" ")
+    
+# 4. 2 4 8 16 32 64 128 256 512 1024
+#a = 2;
+#for i in range(10):
+#    print(a,end=" ")
+#    a *= 2
+    
+#5. 1 3 5 7 9 11 13 15 17 19
+#a=1;
+#for i in range(10):
+#    print(a,end=" " )
+#    a+=2
+
+
+#soal baru
+#1. 1 2 4 7 11 16 22 29 37 46
+#a=1;
+#b=1;
+#for i in range(10):
+#    print(a,end=" " )
+#    a+=b
+#    b+=1
+    
+
+#2. 1 2 5 10 17 26 37 50 85 82
+#a=1
+#b=1
+#for i in range(10):
+#    print(a,end=" " )
+#    a+=b
+#    b+=2
+    
+#3. -5 -2 4 13 25 40 58 79 103 130 160 
+#a=-5
+#b=3
+#for i in range(11):
+#    print(a,end=" " )
+#    a+=b
+#    b+=3
+
+#contoh while
+a=-5
+b=3
+max=11
+x=0
+while (x < max ):
+     print(a,end=" " )
+     a+=b
+     b+=3
+     x+=1
+    
